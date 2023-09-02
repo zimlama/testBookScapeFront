4bd69196b8c7d94a58b48891f5b84d5f53d96674
zimlama <zimlama@gmail.com>
2023-09-02 16:16:08 -0500

    se subre test del Front con cambios de Andres

---
+++ components/LoginGoogle/LoginGoogle.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { CredentialResponse, GoogleLogin } from '@react-oauth/google';
+import decodeJwt from './decodeJwt';
+
+const bookscapeback = process.env.NEXT_PUBLIC_BOOKSCAPEBACK; // Obtiene la URL base del archivo .env.local
+
+export default function LoginGoogle() {
+    const [nombre, setNombre] = useState<string | null>(null);
+
+    function handleError() {
+        console.log("Falla del login Google");
+
+    }
+
+    async function handleSuccess(credentialResponse: CredentialResponse) {
+        console.log("credentialResponse", credentialResponse);
+        if (credentialResponse.credential) {
+            const credenciales = {
+                token: credentialResponse.credential
+            }
+
+            const response = await fetch(`${bookscapeback}/users/login`, {
+                method: "POST",
+                body: JSON.stringify (
+                    credenciales
+                )
+            });
+            const { payload } = decodeJwt(credentialResponse.credential)
+            console.log("payload credential", payload);
+            setNombre(payload.nombre);
+        }
+    }
+
+    return ( 
+        <div>
+            {nombre === null && <GoogleLogin useOneTap onError={handleError} onSuccess={handleSuccess} />}
+            {nombre && <p>El usuario se ha iniciado sesion: {nombre}</p>}
+        </div>
+    )}
